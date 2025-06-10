6fdda288c55d5eca03c755f1a3ae5a2369ef2c02
Raul Migliari <raulmigliari5@gmail.com>
2025-06-10 17:41:36 -0300

    Finalização do desenvolvimento da interface (mockup). Pronto para iniciar a integração com API

--- src/components/CreateTaskForm.tsx
+++ src/components/CreateTaskForm.tsx
@@ -1,7 +1,35 @@
 import { PlusIcon } from "@radix-ui/react-icons"
-import { Box, Button, Dialog, Flex, RadioGroup, Text, TextArea, TextField } from "@radix-ui/themes"
+import { Badge, Box, Button, Dialog, Flex, RadioGroup, Text, TextArea, TextField } from "@radix-ui/themes"
+import type { FormEventHandler } from "react"
+import { z } from "zod"
+
+const CreateTaskSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    status: z.enum(['todo', 'doing', 'done']),
+    priority: z.enum(['low', 'medium', 'high'])
+})
 
 export const CreateTaskForm: React.FC = () => {
+
+    const handleSubmit: FormEventHandler<HTMLFormElement> = async (ev) => {
+
+        ev.preventDefault()
+
+        const formData = new FormData(ev.currentTarget)
+
+        const title = formData.get("title")
+        const description = formData.get("description")
+        const status = formData.get("status")
+        const priority = formData.get("priority")
+
+        ev.currentTarget.reset()
+
+        const taskData = CreateTaskSchema.parse({title, description, status, priority})
+
+        alert(JSON.stringify(taskData))
+    }
+
     return (
         <Dialog.Root>
             <Dialog.Trigger>
@@ -16,7 +44,7 @@ export const CreateTaskForm: React.FC = () => {
                     Adicione novas tarefas ao quadro.
                 </Dialog.Description>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <Flex direction="column" gap="4">
                         <Box maxWidth="32rem">
                             <Box mb="2">
@@ -36,18 +64,18 @@ export const CreateTaskForm: React.FC = () => {
                             <Box>
                                 <Text as="div" mb={"2"}>Situação</Text>
                                 <RadioGroup.Root name="status" defaultValue="todo">
-                                    <RadioGroup.Item value="todo">Para Fazer</RadioGroup.Item>
-                                    <RadioGroup.Item value="doing">Em Progresso</RadioGroup.Item>
-                                    <RadioGroup.Item value="done">Concluída</RadioGroup.Item>
+                                    <RadioGroup.Item value="todo"><Badge color="gray">Para Fazer</Badge></RadioGroup.Item>
+                                    <RadioGroup.Item value="doing"><Badge color="yellow">Em Progresso</Badge></RadioGroup.Item>
+                                    <RadioGroup.Item value="done"><Badge color="green">Concluída</Badge></RadioGroup.Item>
                                 </RadioGroup.Root>
                             </Box>
 
                             <Box>
                                 <Text as="div" mb={"2"}>Prioridade</Text>
                                 <RadioGroup.Root name="priority" defaultValue="low">
-                                    <RadioGroup.Item value="low">Baixa</RadioGroup.Item>
-                                    <RadioGroup.Item value="medium">Média</RadioGroup.Item>
-                                    <RadioGroup.Item value="high">Alta</RadioGroup.Item>
+                                    <RadioGroup.Item value="low"><Badge color="sky">Baixa</Badge></RadioGroup.Item>
+                                    <RadioGroup.Item value="medium"><Badge color="amber">Média</Badge></RadioGroup.Item>
+                                    <RadioGroup.Item value="high"><Badge color="tomato">Alta</Badge></RadioGroup.Item>
                                 </RadioGroup.Root>
                             </Box>
                         </Flex>
